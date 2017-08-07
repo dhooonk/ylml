49dc6b7952b8e88bac38f2770922a6ed77b1262d
suewonseok <dydrlaks@likelion.org>
2017-08-07 21:23:09 +0900

    Cabins 테이블 delete관계 기능보조

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,15 +12,18 @@ class AdminController < ApplicationController
     if user.admin != true
       user.destroy
     else
-
       flash[:warning] = "관리자 계정은 삭제할 수 없습니다."
-
+    end
+    c = Cabinet.find_by(cabins:user.seatNumber)
+    if !!Cabinet.find_by(cabins:user.seatNumber)
+      c.destroy
     end
     redirect_to admin_index_path
   end
 
   def destroy_all ##관리자 계정을 제외한 모든 계정 초기화 액션
     User.where(admin: nil).destroy_all
+    Cabinet.destroy_all
     flash[:warning] = "전체 계정 정보가 초기화 되었습니다."
     redirect_to admin_index_path
   end
