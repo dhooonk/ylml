5f34dcea94ca5b94e74aaa34646d7da706ee78ac
suewonseok <dydrlaks@likelion.org>
2017-08-19 02:56:33 +0900

    신청기간 뷰 업데이트

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   def index
-
+    @limit = Limit.find(1)
+    @limit_ = Limit.find(2)
   end
 
   def new
