500ca29091dddc098fd99a489b080d73cc650693
suewonseok <dydrlaks@likelion.org>
2017-08-31 03:05:34 +0900

    post.index 자잘한 실수 수정

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830063946) do
+ActiveRecord::Schema.define(version: 20170830080224) do
 
   create_table "cabinet_apli_scis", force: :cascade do |t|
     t.integer "user_id"
